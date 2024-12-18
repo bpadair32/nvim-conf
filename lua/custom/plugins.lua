10c7d08d3eeb64f89135da1db2e47688acfb32bd
Brad Adair <bradadair@gmail.com>
2024-12-18 13:18:20 -0500

    Remove ChatGPT plugin that I am no longer using

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,19 +1,5 @@
 local plugins = {
   {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    },
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "pass show openai-key",
-      })
-    end,
-  },
-  {
     "rcarriga/nvim-dap-ui",
     dependencies = "mfussenegger/nvim-dap",
     config = function()
